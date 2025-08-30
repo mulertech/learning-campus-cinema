b572694335b5d51d299ebbd55c0d5421d550a6f9
Sébastien Muler <sebastien.muler@mulertech.net>
2025-08-30 21:27:19 +0200

    Affichage avancé et modification

--- src/Controller/CinemaController.php
+++ src/Controller/CinemaController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Repository\FilmRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class CinemaController extends AbstractController
 {
@@ -16,5 +19,23 @@ final class CinemaController extends AbstractController
         return $this->render('cinema/programmation.html.twig', [
             'films' => $films,
         ]);
+    }
+
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/profil', name: 'app_profil')]
+    public function profil(): Response
+    {
+        return $this->render('cinema/reservations.html.twig');
+    }
+
+    #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/annuler-reservation/{id}', name: 'app_annuler_reservation')]
+    public function annulerReservation(Reservation $reservation, EntityManagerInterface $em): Response
+    {
+        if ($reservation->getUtilisateur()->getId() === $this->getUser()->getId()) {
+            $reservation->setStatut(Reservation::STATUT_ANNULE);
+            $em->flush();
+        }
+        return $this->redirectToRoute('app_profil');
     }
 }
